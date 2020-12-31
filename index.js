2c65d633a66f21029f4d36ca25ddeb66d3a5fd5f
Ali Zakwan <alizakwanansari@gmail.com>
2021-01-01 04:30:09 +0530

    upload file to s3 using multer done

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+require('dotenv/config')
 const express = require('express')
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3')
+const { v4 } = require('uuid')
+const path = require('path')
 const app = express()
-const PORT = 3000
 
-app.post('/upload', (req, res) => {
-    res.send({
-        message:'File Uploaded Successfully',
-    });
-});
+const PORT = process.env.PORT || 3000
+console.log(process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY)
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  sessionToken: process.env.AWS_SESSION_TOKEN,
+})
+
+const storage = multerS3({
+  s3: s3,
+  bucket: process.env.BUCKET_NAME,
+  key: (req, file, cb) => {
+    const ext = path.extname(file.originalname)
+    cb(null, `${v4()}${ext}`)
+  },
+  acl: 'public-read', //comment if you don't want the uploaded file to be publicly accessible
+})
+
+const upload = multer({ storage })
+
+app.post('/upload', upload.array('fileData'), (req, res) => {
+  console.log(req)
+  res.send({
+    message: 'File Uploaded Successfully',
+    uploaded: req.files.length,
+    filesPath: req.files.map((file) => file.location),
+  })
+})
 
 app.listen(PORT, () => {
-    console.log(`Server started on PORT: ${PORT}`);
-});
+  console.log(`Server started on PORT: ${PORT}`)
+})
